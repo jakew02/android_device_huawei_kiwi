e258b2e59579baa524e8f4c26dc3ea6aebedb813
Jake Whatley <jacob.whatley91@gmail.com>
2017-04-26 01:02:18 -0400

    kiwi: add Omni makefile

--- omni_kiwi.mk
+++ omni_kiwi.mk
@@ -1,11 +1,10 @@
-#
 # Copyright (C) 2014 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,6 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_kiwi.mk \
-    $(LOCAL_DIR)/omni_kiwi.mk
+$(call inherit-product, device/huawei/kiwi/full_kiwi.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+PRODUCT_NAME := omni_kiwi
+BOARD_VENDOR := huawei
+
+PRODUCT_GMS_CLIENTID_BASE := android-huawei
