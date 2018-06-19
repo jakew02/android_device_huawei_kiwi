161e8151f9446332fb29804e9a2fa1d86ae0d13d
Jacob Whatley <jake@whatley.us>
2018-06-20 00:21:17 +0200

    kiwi: setup for omni
    Change-Id: I1d0c101465f793b4a8cad9f9c7601d23fbb58db0

--- omni_kiwi.mk
+++ omni_kiwi.mk
@@ -1,23 +1,39 @@
+#
 # Copyright (C) 2014 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-$(call inherit-product, device/huawei/kiwi/full_kiwi.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common.mk)
 
-PRODUCT_NAME := lineage_kiwi
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Inherit from kiwi device
+$(call inherit-product, device/huawei/kiwi/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := kiwi
+PRODUCT_NAME := omni_kiwi
+PRODUCT_BRAND := HONOR
+PRODUCT_MODEL := KIW-L24
+PRODUCT_MANUFACTURER := HUAWEI
+
 BOARD_VENDOR := huawei
 
 PRODUCT_GMS_CLIENTID_BASE := android-huawei
